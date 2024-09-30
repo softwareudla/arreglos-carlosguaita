3b9efd5c8988f198b965e76ba1681d5d64d96f7c
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-09-30 16:40:10 -0500

    cambio1

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+
+    char nombres[5][20];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ingrese el nombre %d: ",i);
+        fflush(stdin);
+        fgets(nombres[i],20,stdin);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s",nombres[i]);
+    }
+    
+    
     return 0;
 }
